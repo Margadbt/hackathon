24e754d110ad922d74551a78398ea1326d044c31
Margadbt <bmargad17@gmail.com>
2024-05-12 07:31:26 +0800

    s

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 "use client"
-import "@/app/app.style.css";
-import RoomCard from "@/components/RoomCard";
-import "@/styles/index.css";
+// import RoomCard from "@/components/RoomCard";
 import RoomCardList from "@/components/RoomCard/RoomCardList";
 import "@/app/app.style.css";
 import SearchBar from "@/components/SearchBar";
