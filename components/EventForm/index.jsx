fbdb100a7cd99817c72f2bd967ebfb0d2b382b93
oyunaaaa <120953222+oyunaaaa@users.noreply.github.com>
2024-05-12 02:48:59 +0800

    form

--- components/EventForm/index.jsx
+++ components/EventForm/index.jsx
@@ -27,70 +27,78 @@ function EventForm() {
     };
 
     return (
-        <div className="request-form-card">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="image">Зураг сонгох</label>
-                <input
-                    type="file"
-                    id="image"
-                    name="image"
-                    accept="image/*"
-                    onChange={handleImageChange}
-                />
-
-                <label htmlFor="name1">Үйл ажиллагааны нэр</label>
-                <textarea
-                    className="form-attr"
-                    id="name1"
-                    name="name1"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    cols="50"
-                    rows="10"
-                    required
-                ></textarea>
-                <label htmlFor="description1">Үйл ажиллагааны талаар дэлгэрэнгүй тайлбар</label>
-                <textarea
-                    className="form-attr"
-                    id="description1"
-                    name="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    cols="50"
-                    rows="10"
-                    required
-                ></textarea>
-
-                <label htmlFor="when2">Хэзээ</label>
-                <div>
-                    <textarea
-                        className="form-attr"
-                        id="when2"
-                        name="when"
-                        value={when}
-                        onChange={(e) => setWhen(e.target.value)}
-                        cols="50"
-                        required
-                    ></textarea>
-
-                </div>
-                <div className="where">
-                    <label htmlFor="where1">Хаана</label>
-                    <div>
-                        <input
-                            className="form-attr"
-                            name="where"
-                            value={where}
-                            onChange={(e) => setWhere(e.target.value)}
-                            required
-                        />
+        <div>
+            <div className="EventFrom"><b>Үйл ажиллагааны бүртгэл</b></div>
+            <div className="request-form-card">
+                <form onSubmit={handleSubmit}>
+                    <div className="column">
+                        <label className="imgName" htmlFor="image">Зураг</label>
+                        <label className="image" htmlFor="image">
+                            Зураг сонгох
+                            <input
+                                type="file"
+                                id="image"
+                                name="image"
+                                accept="image/*"
+                                style={{ display: 'none' }} // Hide the default file input button
+                                onChange={handleImageChange}
+                            />
+                        </label>
                     </div>
-                </div>
-                
-                <button className="submit-button" type="submit">
-                    Бүртгүүлэх
-                </button>
-            </form>
+                    <div>
+                        <label htmlFor="name1">Үйл ажиллагааны нэр</label>
+                        <textarea
+                            className="form-attr"
+                            id="name1"
+                            name="name1"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            cols="50"
+                            rows="10"
+                            required
+                        ></textarea>
+                        <label htmlFor="description1">Үйл ажиллагааны талаар дэлгэрэнгүй тайлбар</label>
+                        <textarea
+                            className="form-attr"
+                            id="description1"
+                            name="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            cols="50"
+                            rows="10"
+                            required
+                        ></textarea>
+
+                        <label htmlFor="when2">Хэзээ</label>
+                        <div>
+                            <textarea
+                                className="form-attr"
+                                id="when2"
+                                name="when"
+                                value={when}
+                                onChange={(e) => setWhen(e.target.value)}
+                                cols="50"
+                                required
+                            ></textarea>
+                        </div>
+                        <div className="where">
+                            <label htmlFor="where1">Хаана</label>
+                            <div>
+                                <input
+                                    className="form-attr"
+                                    name="where"
+                                    value={where}
+                                    onChange={(e) => setWhere(e.target.value)}
+                                    required
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <button className="submit-button" type="submit">
+                        Бүртгүүлэх
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
