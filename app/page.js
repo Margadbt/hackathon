6e1e4eeedb9d9414aa14edd73856e2087bc1b09f
Margadbt <bmargad17@gmail.com>
2024-05-11 13:56:34 +0800

    fixed component card

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import RoomCartComponent from "@/app/RoomCartComponent";
+import RoomCard from "@/components/RoomCard";
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -40,12 +40,7 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <RoomCartComponent></RoomCartComponent>
-        <RoomCartComponent></RoomCartComponent>
-        <RoomCartComponent></RoomCartComponent>
-        <RoomCartComponent></RoomCartComponent>
-        <RoomCartComponent></RoomCartComponent>
-        <RoomCartComponent></RoomCartComponent>
+        <RoomCard></RoomCard>
 
       </div>
     </main>
--- components/RoomCard/index.jsx
+++ components/RoomCard/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 // import classImage from './src/assets/class.jpg';
-import heartGreyIcon from './src/assets/Heart-grey.svg';
-import './src/styles/RoomCartComponent.css';
-import cancelIcon from './src/assets/cancal.png';
+// import heartGreyIcon from './src/assets/Heart-grey.svg';
+// import './src/styles/RoomCartComponent.css';
+// import cancelIcon from './src/assets/cancal.png';
+import "./style.css"
 
-const ClassComponent = () => {
+const RoomCard = () => {
     return (
         <article className="class-section-2">
             <div className="img">
-                <img src='{classImage}' alt="classroom picture" className="image" />
+                {/* <img src='{classImage}' alt="classroom picture" className="image" /> */}
             </div>
             <section className="details">
                 <div className="heartdetailsgrid">
@@ -16,7 +17,7 @@ const ClassComponent = () => {
                         <h3>1-407</h3>
                         <div className="class-type">Лекц</div>
                     </div>
-                    <img src={ heartGreyIcon } alt="like this class" className="heart-grey" />
+                    {/* <img src={ heartGreyIcon } alt="like this class" className="heart-grey" /> */}
                 </div>
                 <ul>
                     <li>03.05-Мя</li>
@@ -30,11 +31,11 @@ const ClassComponent = () => {
             </section>
             <div className="cancel">
                 <button>
-                    <img src={cancelIcon} alt="cancel" />
+                    {/* <img src={cancelIcon} alt="cancel" /> */}
                 </button>
             </div>
         </article>
     );
 };
 
-export default ClassComponent;
+export default RoomCard;
