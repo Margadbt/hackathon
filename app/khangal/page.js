305b58ad3e26dc008ccae3d36f77926475ff2d43
Khangal <118184729+Khangal12@users.noreply.github.com>
2024-05-12 05:11:03 +0800

    a

--- app/khangal/page.js
+++ app/khangal/page.js
@@ -1,11 +1,12 @@
 import EventCard from "@/components/EventCard";
+import EventDetailCard from "@/components/EventDetailCard";
 import EventForm from "@/components/EventForm";
 
 // khangal preview
 export default function Event(){
     return(
     <div>
-<EventForm/>
+<EventDetailCard/>
     </div>
 
 
