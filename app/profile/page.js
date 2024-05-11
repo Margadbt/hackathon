2a6f2075de87c54d105cc4912dc922a02cf5bea6
Chinkhusel14 <xrobertftw@gmail.com>
2024-05-11 16:30:03 +0800

    Profileroomfix

--- app/profile/page.js
+++ app/profile/page.js
@@ -7,7 +7,6 @@ export default function Home() {
     <main class="body">
       <div class="profile-wrapper">
         <div class="left-side">
-          <img src="profileImg" alt="profile-icon" />
           <p>User0021</p>
         </div>
         <div class="right-side">
--- components/ProfileRoomCard/index.jsx
+++ components/ProfileRoomCard/index.jsx
@@ -3,19 +3,21 @@ import "./style.css";
 const ProfileRoomCard = () => {
     return (
         <article className="class-section-2">
-            <div className="img">
-                {/* <img src='{classImage}' alt="classroom picture" className="image" /> */}
+            <div class="image-wrapper">
+            <img
+                src="https://source.unsplash.com/random/400x250/?classroom,lesson&${i}"
+                alt="classroom-picture"
+                class="image"
+              />
             </div>
             <section className="details">
-                <div className="heartdetailsgrid">
+                
                     <div className="font">
                         <h3>1-407</h3>
                         <div className="class-type">Лекц</div>
                     </div>
-                    {/* <img src={ heartGreyIcon } alt="like this class" className="heart-grey" /> */}
-                </div>
                 <ul>
-                    <li>03.05-Мя</li>
+                    <li class="class-day">03.05-Мя</li>
                     <li>
                         <button><span>16:00</span></button>
                     </li>
