c060337d27f4e301803267af71422dbdc35e4d89
Margadbt <bmargad17@gmail.com>
2024-05-12 07:23:41 +0800

    sdf

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,11 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header></Header>
-        <div style={{margin: "7rem 6rem 0 6rem"}}>{children}</div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header></Header>
+          <div style={{ margin: "7rem 6rem 0 6rem" }}>{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
