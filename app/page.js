42f23e9fe1edcfb2c2afa0d2a5a90c0e5011245b
oyunaaaa <120953222+oyunaaaa@users.noreply.github.com>
2024-05-11 15:00:10 +0800

    request shu

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import RoomCard from "@/components/ProfileRoomCard";
+import RequestForm from "@/components/Request";
 import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
         <RoomCard></RoomCard>
+        <RequestForm></RequestForm>
       </div>
     </main>
   );
