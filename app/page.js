37cd1162ac80955e961450138841c7c0a154d51a
Chinkhusel14 <xrobertftw@gmail.com>
2024-05-11 15:32:48 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,23 +1,18 @@
-import styles from "./class.css";
 import RoomCard from "@/components/RoomCard";
 import ProfileRoomCard from "@/components/ProfileRoomCard";
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
-
-          <RoomCard></RoomCard>
-          <RoomCard></RoomCard>
-          <RoomCard></RoomCard>
-          <RoomCard></RoomCard>
-          <ProfileRoomCard></ProfileRoomCard>
-          <ProfileRoomCard></ProfileRoomCard>
-          <ProfileRoomCard></ProfileRoomCard>
-          <ProfileRoomCard></ProfileRoomCard>
-          <ProfileRoomCard></ProfileRoomCard>
-
-
-
+        <RoomCard></RoomCard>
+        <RoomCard></RoomCard>
+        <RoomCard></RoomCard>
+        <RoomCard></RoomCard>
+        <ProfileRoomCard></ProfileRoomCard>
+        <ProfileRoomCard></ProfileRoomCard>
+        <ProfileRoomCard></ProfileRoomCard>
+        <ProfileRoomCard></ProfileRoomCard>
+        <ProfileRoomCard></ProfileRoomCard>
       </div>
     </main>
   );
