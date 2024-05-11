f4699aee2ad92d903ed0f2549851039410caa5b4
Khangal <118184729+Khangal12@users.noreply.github.com>
2024-05-11 15:27:52 +0800

    a

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import RoomCard from "@/components/ProfileRoomCard";
 
 import styles from "./page.module.css";
+import EventCard from "@/components/EventCard";
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
         <RoomCard></RoomCard>
+        <EventCard></EventCard>
       </div>
     </main>
   );
