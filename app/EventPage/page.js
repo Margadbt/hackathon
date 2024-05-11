9fe124bb24de2ba8a7154c7a13ae4ff3618257ac
beeembe <117098491+belgeee@users.noreply.github.com>
2024-05-11 21:48:46 +0800

    event css fixed

--- app/EventPage/page.js
+++ app/EventPage/page.js
@@ -7,39 +7,59 @@ import RoomCard from "@/components/RoomCard";
 export default function EventPage(){
     return(
     <div className="Event-main">
-        <div className="Event-cards">
+   <div className="Event-cards">
 <EventCard></EventCard>
 <EventCard></EventCard>
 <EventCard></EventCard>
 <EventCard></EventCard>
-
-</div>
+        </div>
 <div className="Event-sidebar">
 <aside class="index-aside">
-        <section>
-          <h2>Онцлох ангиуд</h2>
-          <a href="class.html" class="class-section-3">
+    <section>
+        <h2>Онцлох ангиуд</h2>
+        <a href="class.html" className="class-section-9">
             <RoomCard/>
-          </a>
-          <a href="class.html" class="class-section-3">
-          <RoomCard/>
-          </a>
-          <a href="class.html" class="class-section-3">
-          <RoomCard/>
-          </a>
-          <a href="class.html" class="class-section-3">
-          <RoomCard/>
 
-          </a>
-          <a href="class.html" class="class-section-3">
-      <RoomCard/>
+        </a>
+        <a href="class.html" className="class-section-9">
+            <RoomCard/>
 
-          </a>
-          <a href="class.html" class="class-section-3">
-          <RoomCard/>
-          </a>
-        </section>
-      </aside>
+        </a>
+        <a href="class.html" className="class-section-9">
+            <RoomCard/>
+
+        </a>
+        <a href="class.html" className="class-section-9">
+            <RoomCard/>
+
+        </a>
+        <a href="class.html" className="class-section-9">
+            <RoomCard/>
+
+        </a>
+        <a href="class.html" className="class-section-9">
+            <RoomCard/>
+
+        </a>
+        {/*    <a href="class.html" class="class-section-3">*/}
+        {/*    <RoomCard/>*/}
+        {/*    </a>*/}
+        {/*    <a href="class.html" class="class-section-3">*/}
+        {/*    <RoomCard/>*/}
+        {/*    </a>*/}
+        {/*    <a href="class.html" class="class-section-3">*/}
+        {/*    <RoomCard/>*/}
+
+        {/*    </a>*/}
+        {/*    <a href="class.html" class="class-section-3">*/}
+        {/*<RoomCard/>*/}
+
+        {/*    </a>*/}
+        {/*    <a href="class.html" class="class-section-3">*/}
+        {/*    <RoomCard/>*/}
+        {/*    </a>*/}
+    </section>
+</aside>
 </div>
     </div>
 
