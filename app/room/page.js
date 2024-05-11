73d7e0dc25fbfd0ba01cb9bf76602be95d958ace
Margadbt <bmargad17@gmail.com>
2024-05-12 01:15:58 +0800

    unuuu shunu buten noirtoi honoh gej blashd

--- app/room/page.js
+++ app/room/page.js
@@ -1,36 +1,36 @@
-'use client'
-import React, { useState } from 'react';
+"use client"
+
 import RequestForm from "@/components/RequestForm";
 import RoomDetailCard from "@/components/RoomDetailCard";
 import "./style.css";
 import Calendar from "react-calendar";
 import Image from "next/image";
-import rooms from "../../assets/rooms.jpg"
+import rooms from "../../assets/rooms.jpg";
 import RoomCard from "@/components/RoomCard";
 import AvailableTimeSection from "@/components/AvailableTimeSection";
-import "react-calendar/dist/Calendar.css";
+import { useState } from "react";
 
 export default function Room() {
-    const [fixedDate, setFixedDate] = useState(new Date());
+  const [fixedDate, setFixedDate] = useState(new Date());
 
-    function handleDateChange(date) {
-        setFixedDate(date);
-        console.log(date)
-    }
-
-    return (
-        <main>
-            <div className="{roomDetailHead}">
-                <p>7-203</p>
-                <RoomDetailCard />
-            </div>
-            <div className="{styles.requestRoomSection}">
-                <Calendar
-                    onChange={handleDateChange}
-                    value={fixedDate}
-                />
-                <RequestForm />
-            </div>
-        </main>
-    );
+  function handleDateChange(date) {
+    setFixedDate(date);
+    console.log(date);
+  }
+  return (
+    <main>
+      <div className="room-detail-head-container">
+        <div className="room-detail-head-image">
+          <p>7-203</p>
+          <Image className="room-image" src={rooms} />
+        </div>
+        <RoomDetailCard />
+      </div>
+      <div className="request-room-section">
+        <Calendar onChange={handleDateChange} value={fixedDate} />
+        <AvailableTimeSection />
+        <RequestForm />
+      </div>
+    </main>
+  );
 }
