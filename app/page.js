5914bf512b209cf49f790d8598154aafb812f1bd
oyunaaaa <120953222+oyunaaaa@users.noreply.github.com>
2024-05-11 14:36:58 +0800

    request comp, login

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import RoomCard from "@/components/RoomCard";
+
 export default function Home() {
+  const handleLogin = async (email, password) => {
+   
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -40,9 +44,24 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <RoomCard></RoomCard>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        {}
 
       </div>
+
+      {}
+      
     </main>
   );
 }
