5b93253037ee2768d748200cd621ca4d8825fd7a
Margadbt <bmargad17@gmail.com>
2024-05-11 19:24:22 +0800

    montserrat

--- app/room/page.js
+++ app/room/page.js
@@ -1,16 +1,18 @@
 import RequestForm from "@/components/RequestForm";
 import RoomDetailCard from "@/components/RoomDetailCard";
-import "./style.css"
+import "./style.css";
+import Calendar from "react-calendar";
 
 export default function Room() {
   return (
     <main>
-        <div className="room-detail-head">
-          <p>7-203</p>
-          {/* image */}
+      <div className="room-detail-head">
+        <p>7-203</p>
+        {/* image */}
         <RoomDetailCard />
-        </div>
+      </div>
       <div className="request-room-section">
+        <Calendar></Calendar>
         <RequestForm />
       </div>
     </main>
--- components/RoomDetailCard/index.jsx
+++ components/RoomDetailCard/index.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 
-import "./style.css"
+import "./style.css";
 export default function RoomDetailCard() {
   return (
-      <div className="room-detail-card-container">
-        <div className="room-detail-card">
-          <div style={{borderRight: "2px solid rgb(0, 0, 0, 0.1)"}}>Лекц</div>
-          <div style={{borderRight: "2px solid rgb(0, 0, 0, 0.1)"}}>
-            <p>Суудлын тоо</p>55
-          </div>
-          <div>
-            <p>Проектор</p>Icon
-          </div>
-            <Calendar></Calendar>
+    <div className="room-detail-card-container">
+      <div className="room-detail-card">
+        <div style={{ borderRight: "2px solid rgb(0, 0, 0, 0.1)" }}>Лекц</div>
+        <div style={{ borderRight: "2px solid rgb(0, 0, 0, 0.1)" }}>
+          <p>Суудлын тоо</p>55
+        </div>
+        <div>
+          <p>Проектор</p>Icon
         </div>
       </div>
+    </div>
   );
 }
