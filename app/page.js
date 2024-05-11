0fb1240f9aa8aece121aaff3de33d88bb8f3ebd8
Chinkhusel14 <xrobertftw@gmail.com>
2024-05-11 21:51:17 +0800

    a

--- app/page.js
+++ app/page.js
@@ -1,19 +1,46 @@
 
 
-
 export default function Home() {
   return (
     <main>
-      <div>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
-        <RoomCard></RoomCard>
+      <div class="main-wrapper">
+        <div class="left-side">
+          <div class="filter-bar"></div>
+          <div class="class-wrapper">
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+            <RoomCard></RoomCard>
+          </div>
+        </div>
+        <aside class="index-aside">
+          <section>
+            <h2>Онцлох ангиуд</h2>
+            <a href="class.html" class="class-section-3">
+              <RoomCard />
+            </a>
+            <a href="class.html" class="class-section-3">
+              <RoomCard />
+            </a>
+            <a href="class.html" class="class-section-3">
+              <RoomCard />
+            </a>
+            <a href="class.html" class="class-section-3">
+              <RoomCard />
+            </a>
+            <a href="class.html" class="class-section-3">
+              <RoomCard />
+            </a>
+            <a href="class.html" class="class-section-3">
+              <RoomCard />
+            </a>
+          </section>
+        </aside>
       </div>
     </main>
   );
