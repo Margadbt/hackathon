8c3e38dadcd308382093939f4cfa5db06c942ff9
oyunaaaa <120953222+oyunaaaa@users.noreply.github.com>
2024-05-11 14:40:33 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,39 +1,11 @@
-import styles from "./page.module.css";
-<<<<<<< HEAD
-
-=======
 import RoomCard from "@/components/ProfileRoomCard";
->>>>>>> 32f2970331180a5748973688d303745b0ae989bf
+import styles from "./page.module.css";
 export default function Home() {
-  const handleLogin = async (email, password) => {
-   
-  };
-
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
-<<<<<<< HEAD
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        {}
-
-=======
         <RoomCard></RoomCard>
->>>>>>> 32f2970331180a5748973688d303745b0ae989bf
       </div>
-
-      {}
-      
     </main>
   );
 }
