46df2540be4cbfc0b782de8a56c2d86c4ea35c45
beeembe <117098491+belgeee@users.noreply.github.com>
2024-05-12 08:02:11 +0800

    baahan um nemew boldoo

--- components/RequestedEventClass/index.jsx
+++ components/RequestedEventClass/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import './style.css'
-const RequestedEventClass = () => {
+const RequestedEventClass = ({data}) => {
 
 
     return (
         <div>
                 <div className="admin-container">
+                    <p><b>{data.date}</b></p>
                     <div className="request-day">
-                        <h2>03/11 Даваа</h2>
+                        <h2 ><b></b></h2>
                         <div className="request-item">
                             <div className="time-room">
                                 <p>1-407 · Лекц</p>
                                 <p>16:00</p>
                             </div>
                             <div className="status">
-                                <p>Клубын үүзалт</p>
+                                <p>{data.purpose}</p>
                                 <p>25 хүн</p>
                             </div>
                             <div className="actions">
-                                <button className="approve">✔️</button>
+                                <button className="approve">✅️</button>
                                 <button className="reject">❌</button>
                             </div>
                         </div>
